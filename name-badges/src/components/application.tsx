fa4c9b901adfc7b8b804594342699e3de7ec3a98
howardphungdev <phungthinh0946519356@gmail.com>
2023-12-07 09:30:55 +0700

    init projects react w ts

---
+++ name-badges/src/components/application.tsx
@@ -0,0 +1,10 @@
+// import ControlPanel from './controls';
+import NameBadge from './name-badge';
+
+const Application = () => (
+  <main className="application">
+    <NameBadge greeting="helloo" name="haha" />
+  </main>
+);
+
+export default Application;
