cd3508c420b9b9b1935ed9fe935127ceea34f639
howardphungdev <phungthinh0946519356@gmail.com>
2023-12-08 08:15:08 +0700

    feat: typing component state application

--- accident-counter/src/components/counter.tsx
+++ accident-counter/src/components/counter.tsx
@@ -1,16 +1,35 @@
+import { FormEvent, useState } from 'react';
+
 const Counter = () => {
+  const [count, setCount] = useState<number>(0);
+  const [draftCounter, setDraftCounter] = useState<number>(0);
+
+  function onUpdateCounterHandler(e: FormEvent) {
+    e.preventDefault();
+    setCount(draftCounter);
+    setDraftCounter(0);
+  }
+
   return (
-    <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
+    <section className="flex w-2/3 flex-col items-center gap-8 border-4 border-primary-500 bg-white p-8 shadow-lg">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{count}</p>
       <div className="flex gap-2">
-        <button>➖ Decrement</button>
-        <button>🔁 Reset</button>
-        <button>➕ Increment</button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          ➖ Decrement
+        </button>
+        <button onClick={() => setCount(0)}>🔁 Reset</button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          ➕ Increment
+        </button>
       </div>
       <div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="number" value={0} />
+        <form onSubmit={onUpdateCounterHandler}>
+          <input
+            type="number"
+            onChange={(e) => setDraftCounter(e.target.valueAsNumber)}
+            value={draftCounter}
+          />
           <button type="submit">Update Counter</button>
         </form>
       </div>
