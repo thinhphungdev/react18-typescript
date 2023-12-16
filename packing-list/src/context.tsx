637a0afd8db584cb9e0fb84308f9681aa78b1e5b
howardphungdev <phungthinh0946519356@gmail.com>
2023-12-16 15:19:41 +0700

    typing context api refactor

--- packing-list/src/context.tsx
+++ packing-list/src/context.tsx
@@ -7,12 +7,54 @@ import {
   updateItem,
 } from './lib/items';
 
-const ItemsContext = createContext(null);
+type ItemsState = {
+  items: Item[];
+  unpackedItems: Item[];
+  packedItems: Item[];
+  add: (name: string) => void;
+  update: (id: string, updates: any) => void;
+  remove: (id: string) => void;
+  markAllAsUnpacked: () => void;
+};
+
+export const ItemsContext = createContext({} as ItemsState);
 
 const ItemsProvider = ({ children }: PropsWithChildren) => {
-  const [items, setItems] = useState<Item[]>(getInitialItems());
+  const [items, setItems] = useState(getInitialItems());
 
-  return <ItemsContext.Provider value={null}>{children}</ItemsContext.Provider>;
+  const add = (name: string) => {
+    const item = createItem(name);
+    setItems([...items, item]);
+  };
+
+  const update = (id: string, updates: any) => {
+    setItems(updateItem(items, id, updates));
+  };
+
+  const remove = (id: string) => {
+    setItems(removeItem(items, id));
+  };
+
+  const unpackedItems = filterItems(items, { packed: false });
+  const packedItems = filterItems(items, { packed: true });
+
+  const markAllAsUnpacked = () => {
+    return setItems(items.map((item) => ({ ...item, packed: false })));
+  };
+
+  const value = {
+    items,
+    unpackedItems,
+    packedItems,
+    add,
+    update,
+    remove,
+    markAllAsUnpacked,
+  };
+
+  return (
+    <ItemsContext.Provider value={value}>{children}</ItemsContext.Provider>
+  );
 };
 
 export default ItemsProvider;
